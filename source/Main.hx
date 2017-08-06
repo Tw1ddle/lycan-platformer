e8803c8fb5c962c299b911f49fc12a6dc960d5fe
sam <sam@silver42.karoo.co.uk>
2017-08-06 18:04:23 +0100

    Remove submodule, we'll just use haxelib git directly, get something building

--- source/Main.hx
+++ source/Main.hx
@@ -3,9 +3,11 @@ package;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
+import lycan.states.LycanRootState;
+
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(0, 0, LycanDemoRootState, 1, 60, 60, true, false));
+		addChild(new FlxGame(0, 0, LycanRootState, 1, 60, 60, true, false));
 	}
 }
