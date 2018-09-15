221987b53bcaaf5c571f6e19634dc6b5f0143531
sam <sam@silver42.karoo.co.uk>
2018-09-15 16:06:59 +0100

    Rough code to ease camera zoom level

--- source/RootState.hx
+++ source/RootState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.system.scaleModes.RatioScaleMode;
 import lycan.states.LycanRootState;
+import flixel.math.FlxMath;
 
 class RootState extends LycanRootState {
 	public function new() {
@@ -23,6 +24,13 @@ class RootState extends LycanRootState {
 		if (FlxG.keys.justPressed.R) {
 			openPlaystate();
 		}
+		
+		if (FlxG.keys.justPressed.O) {
+			targetZoom += 0.5;
+		} else if (FlxG.keys.justPressed.P) {
+			targetZoom -= 0.5;
+		}
+		easeCameraZoom();
 	}
 	
 	private function openPlaystate():Void {
@@ -32,5 +40,22 @@ class RootState extends LycanRootState {
 		//openSubState(new AutotilingState());
 		
 		openSubState(new PhysicsTestState());
+	}
+	
+	// Some code for easing the zoom level
+	private var actualZoom(get, set):Float;
+	private function get_actualZoom():Float {
+		return FlxG.camera.zoom;
+	}
+	private function set_actualZoom(zoom:Float):Float {
+		FlxG.camera.zoom = zoom;
+		return FlxG.camera.zoom;
+	}
+	private var targetZoom(default, set):Float = 1;
+	private function set_targetZoom(zoom:Float):Float {
+		return this.targetZoom = FlxMath.bound(zoom, 0.05, 3);
+	}
+	private function easeCameraZoom():Void {
+		actualZoom = FlxMath.lerp(actualZoom, targetZoom, 0.02);
 	}
 }
