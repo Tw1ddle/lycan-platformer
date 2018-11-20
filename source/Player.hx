68df6f930ad5974d6cdc8ea1535782e86b179178
Joe Williamson <huruey@gmail.com>
2018-11-20 17:04:03 +0000

    Initial pass at player feet and grounding

--- source/Player.hx
+++ source/Player.hx
@@ -1,5 +1,8 @@
 package;
 
+import box2D.collision.B2WorldManifold;
+import flixel.math.FlxPoint;
+import flixel.math.FlxVector;
 import box2D.collision.shapes.B2MassData;
 import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Fixture;
@@ -12,6 +15,7 @@ import lycan.util.GraphicUtil;
 import lycan.world.components.Groundable;
 import lycan.world.components.PhysicsEntity;
 import box2D.dynamics.B2FilterData;
+import box2D.dynamics.B2Fixture;
 
 class MovingPlatform {}
 
@@ -21,6 +25,8 @@ class Player extends LSprite implements PhysicsEntity implements Groundable {
 	var runSpeed:Float = 5;
 	var movingPlatforms:Array<MovingPlatform>;
 	var currentMovingPlatform:MovingPlatform;
+	
+	var feetFixture:B2Fixture;
 	
 	public function new(x:Int, y:Int, width:Int, height:Int) {
 		super(x, y);
@@ -37,22 +43,20 @@ class Player extends LSprite implements PhysicsEntity implements Groundable {
 		physics.init();
 		physics.fixedRotation = true;
 		physics.sleepingAllowed = false;
-		physics.addRectangularShapeAdv(width, height, 0, 0, 50, new B2FilterData(), 10, false, 0, this);
-		
-		//box2D.collision.
-		//physics.body.getContactList()
-		
-		// Sensor for checking if grounded
-		var groundedSensor = physics.addCircleShapeAdv(10, 0, height / 2, 50, new B2FilterData(), 10, true, 0, this);
+		physics.addRectangularShapeAdv(width, height - width / 2, 0, -width / 4, 50, new B2FilterData(), 10, false, 0, this);
+		feetFixture = physics.addCircleShapeAdv(width / 2, 0, (height - width) / 2, 0, new B2FilterData(), 0, false, 0, this);
 	}
 	
 	override public function update(dt:Float):Void {
 		super.update(dt);
 		
-		if (FlxG.keys.anyPressed([FlxKey.A, FlxKey.LEFT])) {
+		var running:Bool = false;
+		if (physics.linearVelocityX > -runSpeed && FlxG.keys.anyPressed([FlxKey.A, FlxKey.LEFT])) {
 			physics.applyImpulse(-runImpulse, 0);
-		} else if (FlxG.keys.anyPressed([FlxKey.D, FlxKey.RIGHT])) {
+			running = true;
+		} else if (physics.linearVelocityX < runSpeed && FlxG.keys.anyPressed([FlxKey.D, FlxKey.RIGHT])) {
 			physics.applyImpulse(runImpulse, 0);
+			running = true;
 		}
 		
 		
@@ -63,18 +67,45 @@ class Player extends LSprite implements PhysicsEntity implements Groundable {
 			animation.play("idle");
 		}
 		
-		if (FlxG.keys.anyJustPressed([FlxKey.W, FlxKey.UP])) {
-			if (true || groundable.isGrounded) {// TODO condition
+		var c = physics.body.getContactList();
+		groundable.forceGrounded = false;
+		var i:Int = 0;
+		while (c != null) {
+			// Todo where were the extra contacts coming from without this!?
+			if (c.contact.isTouching()) {
+				var wm = new B2WorldManifold();
+				c.contact.getWorldManifold(wm);
+				var normal:B2Vec2 = wm.m_normal;
+				var fn:FlxVector = FlxVector.get(normal.x, normal.y);
+				var fa:Bool = c.contact.getFixtureA().m_body == physics.body;
+				FlxG.watch.addQuick("normal", normal.x + " " + normal.y);
+				FlxG.watch.addQuick("body a", fa);
+				FlxG.watch.addQuick("body b", c.contact.getFixtureB().m_body == physics.body);
+				var a:Float = fn.angleBetween(FlxPoint.weak(0, fa ? 0 : -1));
+				fn.put();
+				
+				if (Math.abs(a) < 30) groundable.forceGrounded = true;
+				
+				i++;
+			}
+			c = c.next;
+		}
+		FlxG.watch.addQuick("contacts", i);
+		
+		if (groundable.isGrounded && !running) {
+			feetFixture.setFriction(100);
+		} else {
+			feetFixture.setFriction(0);
+		}
+		
+		if (FlxG.keys.anyPressed([FlxKey.W, FlxKey.UP])) {
+			if (groundable.isGrounded) {// TODO condition
 				physics.linearVelocityY = jumpSpeed;
 			}
 		}
 		
 		//physics.body.getContactList
 		// TODO double jumps + not hanging on walls
-		
-		if (groundable.isGrounded) {
-			trace(groundable.isGrounded); // (actually before it updates, since it's appended)
-		}
 	}
 	
 	public function run() {
